e4d4e078f1ebe84bee001bc0d009250e33579502
hildxd <hildxd@outlook.com>
2024-01-19 14:37:54 +0800

    feat: add swagger and rapidoc

--- apps/api/src/main.rs
+++ apps/api/src/main.rs
@@ -3,13 +3,77 @@ extern crate rocket;
 
 #[cfg(test)]
 mod tests;
+use rocket::form::FromForm;
+use rocket::{get, post, serde::json::Json};
+use rocket_okapi::okapi::schemars;
+use rocket_okapi::okapi::schemars::JsonSchema;
+use rocket_okapi::settings::UrlObject;
+use rocket_okapi::{openapi, openapi_get_routes, rapidoc::*, swagger_ui::*};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, JsonSchema)]
+#[serde(rename_all = "camelCase")]
+struct User {
+    /// a unique user identifier.
+    user_id: u64,
+    /// the current username of the user.
+    username: String,
+    #[schemars(example = "example_email")]
+    email: Option<String>,
+}
+
+fn example_email() -> &'static str {
+    "test@example.com"
+}
+
+/// # Get all users
+///
+/// Returns all users in the system.
+#[openapi(tag = "Users")]
+#[get("/user")]
+fn get_all_users() -> Json<Vec<User>> {
+    Json(vec![User {
+        user_id: 42,
+        username: "bob".to_owned(),
+        email: None,
+    }])
+}
 
 #[get("/<name>/<age>")]
 fn hello(name: &str, age: u8) -> String {
     format!("Hello, {} year old named {}!", age, name)
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello])
+#[rocket::main]
+async fn main() {
+    let launch_result = rocket::build()
+        .mount("/", openapi_get_routes![get_all_users])
+        .mount(
+            "/swagger-ui/",
+            make_swagger_ui(&SwaggerUIConfig {
+                url: "../openapi.json".to_owned(),
+                ..Default::default()
+            }),
+        )
+        .mount(
+            "/rapidoc/",
+            make_rapidoc(&RapiDocConfig {
+                general: GeneralConfig {
+                    spec_urls: vec![UrlObject::new("General", "../openapi.json")],
+                    ..Default::default()
+                },
+                hide_show: HideShowConfig {
+                    allow_spec_url_load: false,
+                    allow_spec_file_load: false,
+                    ..Default::default()
+                },
+                ..Default::default()
+            }),
+        )
+        .launch()
+        .await;
+    match launch_result {
+        Ok(_) => println!("Rocket shut down gracefully."),
+        Err(err) => println!("Rocket had an error: {}", err),
+    };
 }
